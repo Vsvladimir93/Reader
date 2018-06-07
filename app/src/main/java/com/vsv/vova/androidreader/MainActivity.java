744e04b5bb535f803b7e84140d04068a76205d5a
Vsvladimir93 <vsvladimir93@gmail.com>
2018-06-08 00:14:50 +0300

    first commit from note

---
+++ app/src/main/java/com/vsv/vova/androidreader/MainActivity.java
@@ -0,0 +1,31 @@
+package com.vsv.vova.androidreader;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnFind;
+    public static final int READ_REQUEST_CODE = 42;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnFind = (Button) findViewById(R.id.button);
+        btnFind.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+}
